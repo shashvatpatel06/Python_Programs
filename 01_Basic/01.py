8d4951b0f4b9459880ec6ca08d4883b7b5739182
shashvatpatel06 <shashvatpatel006@gmail.com>
2025-01-25 15:18:24 +0530

    Update 01.py

--- 01_Basic/01.py
+++ 01_Basic/01.py
@@ -1,1 +1,100 @@
+# a = int(input("Enter a number: "))
+# b =int(input("Enter another number: "))
+# print("Sum of two numbers:","a+b =",a+b)
+# print("Subtraction of two numbers:","a-b =",a-b)
+# print("Multiplication of two numbers:","a*b =",a*b)
+# print("Division:","a/b =",a/b)
+
+# h = int(input("Enter hours: "))
+# print("Hours into Minutes:",60*h,"min")
+# m = int(input("Enter minutes:"))
+# print("Minutes into Hours:",m/60,"hrs")
+
+# r = int(input("Enter Rupees(1$ = 48 Rs.): "))
+# print("Rupees to Dollars:","$",r/48)
+# d = int(input("Enter dollars(1$ = 48 Rs.): "))
+# print("Dollars to Rupees:",d*48,"Rs.")
+# print("Dollars to Pounds(1$ = 48 Rs. & 1 pound = 70 Rs.):",d*(48/70))
+
+# grams = int(input("Enter grams:"))
+# print("grams into kgs:",grams/1000)
+# kgs = int(input("Enter kgs:"))
+# print("kgs into grams:",1000*kgs)
+
+# byts = int(input("Enter bytes: "))
+# Kb = byts/1024
+# Mb = Kb/1024
+# Gb = Mb/1024
+# print("Bytes into Kb:",Kb)
+# print("Bytes in Mb:",Mb)
+# print("Bytes into Gb:",Gb)
+
+# C = int(input("Enter temperature in Celcius:"))
+# print("F = (9/5 * C) + 32 =",(9/5 * C) + 32)
+# F = int(input("Enter temperature in Fahrenheit:")) 
+# print("C = (5/9) * (F - 32) =",(5/9) * (F - 32))\
+
+# p = float(input("Enter Principal amount: "))
+# r = float(input("Enter rate: "))
+# t = int(input("Enter time period: "))
+# i = (p*r*t)/100
+# print("Interest = (p*r*t)/100 =",i)
+
+# l = float(input("Enter length of square: "))
+# print("Perimeter of Square is",4*l)
+# print("Area of Square:",l*l)
+
+# l = float(input("Enter length of rectangle: "))
+# b = float(input("Enter breadth of rectangle: "))
+# print("Perimeter of Rectangle is",2*l+2*b)
+# print("Area of Rectangle:",l*b)
+
+# r = float(input("Enter Radius of Circle: "))
+# print("Area of Circle:",(22*r*r)/7)
+
+# bl = float(input("Enter base length of Triangle: "))
+# h = float(input("Enter heigth of Triangle: "))
+# print("Area of Triangle:",(bl*h)/2)
+
+# gs = float(input("Enter gross salary: "))
+# allowance = (10*s)/100
+# deduction = (3*s)/100
+# print("Net salary is",gs+allowance-deduction)
+
+# gross_sales = float(input("Enter gross sales: "))
+# print("Net Profit is",gross_sales-(10*gross_sales)/100)
+
+# s1 = float(input("Enter marks of Subject_1: "))
+# s2 = float(input("Enter marks of Subject_2: "))
+# s3 = float(input("Enter marks of Subject_3: "))
+# print("Total marks:",s1+s2+s3)
+# print("Avg marks of 3 Subjects:",(s1+s2+s3)/3)
+
+# Swap two values
+a,b = 10,22
+a,b = b,a
+print("a =",a,"b =",b)
+
+a,b = 11,34
+temp = a
+a = b
+b = temp
+print("a =",a,"b =",b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
