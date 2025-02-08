7c728cfb5f4eaa9296046b132dc4dec8b2802880
shashvatpatel06 <shashvatpatel006@gmail.com>
2025-02-08 10:13:49 +0530

    Update q2.py

--- 05_lists/q2.py
+++ 05_lists/q2.py
@@ -1,7 +1,7 @@
 import random
 lst = [random.randrange(0,20) for _ in range(0,20)]
-n = int(input("Enter a number: "))
 print(lst)
+n= int(input("Enter a number: "))
 if(n in lst):
     for i in range(0,20):
         if(lst[i]==n):
