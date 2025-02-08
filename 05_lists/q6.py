f753939ca50c30ad3eed86d938f7414df3db593d
shashvatpatel06 <shashvatpatel006@gmail.com>
2025-02-08 12:31:11 +0530

    Create q6.py

---
+++ 05_lists/q6.py
@@ -0,0 +1,14 @@
+# 6.)	Convert list of temperatures in Fahrenheit degrees 
+# to equivalent Celsius degrees
+
+# print("Enter temperatures in Farenheit:")
+# lst= [float(input()) for i in range(0,1)] 
+
+# for i in lst:
+#     i = float((5/9)*(i-32))
+#     print(i)
+# print("Temperatures in Celsius",lst)
+
+# for i in range(0,5):
+#     lst[i] = float((5/9)*(lst[i]-32))
+# print("Temperatures in Celsius",lst)
