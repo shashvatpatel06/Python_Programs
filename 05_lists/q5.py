4b30b17276eb60adbce4d0e7aa6cc6ad984f4859
shashvatpatel06 <shashvatpatel006@gmail.com>
2025-02-08 12:30:18 +0530

    Create q5.py

---
+++ 05_lists/q5.py
@@ -0,0 +1,8 @@
+# 5.)A list contains 5 strings. Convert all these strings to uppercase.
+# lst = ["Shashvat","Patel","Computer Science","Pdeu","Ahmedabad"]
+# for i in range(0,5):
+#     lst[i] = lst[i].upper()
+# print(lst)    
+# for i in lst:
+#     i.upper()
+# print(lst)
