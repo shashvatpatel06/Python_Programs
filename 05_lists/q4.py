4d13792a3cfc2fb3c9f5c5f6b497e5ebcb9ca613
shashvatpatel06 <shashvatpatel006@gmail.com>
2025-02-08 12:29:38 +0530

    Create q4.py

---
+++ 05_lists/q4.py
@@ -0,0 +1,16 @@
+# 4.)Generate 30 random numbers and put them in a list. 
+# Create two more lists – one containing only +ve numbers and another with –ve nos.
+
+# lst = [random.randrange(-100,101) for _ in range(0,30)]
+# print("Original List",lst)
+# pos_num =[]
+# neg_num = []
+# for i in lst:
+#     if(i>0):
+#         pos_num.append(i)
+#     elif(i<0):
+#         neg_num.append(i)
+#     else:
+#         continue
+# print("Postive numbers List:",pos_num)
+# print("Negative numbers List",neg_num)
