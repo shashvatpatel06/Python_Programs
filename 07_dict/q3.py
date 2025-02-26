02a4b6db8e6d1eda95f698b74eb0dd4e91a94db9
shashvatpatel06 <shashvatpatel006@gmail.com>
2025-02-26 23:22:28 +0530

    Update q3.py

--- 07_dict/q3.py
+++ 07_dict/q3.py
@@ -1,1 +1,12 @@
+company = {(1, 308): 50000, (2, 268): 60000, (3, 290): 40000, (1, 261): 55000, (2, 245): 70000}
+salary = {}
 
+for (d, r), s in company.items():
+    if d not in salary:
+        salary[d] = {"total":s,"max": s, "min": s}  
+    else:
+        salary[d]["max"] = max(salary[d]["max"], s)
+        salary[d]["min"] = min(salary[d]["min"], s)
+        salary[d]["total"] += s 
+    
+print(salary)
