f7d85cc079ce577bf3f2843b798c32a87ec5e130
Shashvat Patel <shashvatpatel006@gmail.com>
2025-02-26 23:56:57 +0530

    Update q1.py

--- 03_string/q1.py
+++ 03_string/q1.py
@@ -1,1 +1,7 @@
+# 1.)count no. of vowels in string
+
+str = input("Enter a string: ")
+count=0
+count = str.count('a')+str.count('e')+str.count('i')+str.count('o')+str.count('u')
+print("vowels in entered string is",count)
 
