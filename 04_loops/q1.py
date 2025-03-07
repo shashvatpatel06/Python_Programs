cbc97610fa8ee9b7cbb7f8ba9703fdff5abd438f
Shashvat Patel <shashvatpatel006@gmail.com>
2025-03-07 16:10:57 +0530

    Create q1.py

---
+++ 04_loops/q1.py
@@ -0,0 +1,9 @@
+from math import pow
+# 1.)print all alphabets in upper and lower case
+
+# for i in range(65,91):
+#     print(chr(i),end=" ")
+# print()
+# for i in range(97,123):
+#     print(chr(i),end=" ")
+
