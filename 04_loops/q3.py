400a85a4a3005cd7d135ee3d299bb51c4e2eeba9
Shashvat Patel <shashvatpatel006@gmail.com>
2025-03-07 16:13:56 +0530

    Create q3.py

---
+++ 04_loops/q3.py
@@ -0,0 +1,10 @@
+# 3.)count no. of alphabets and digits in given string
+
+# str = input("Enter a string: ")
+# digits,alphabets = 0,0
+# for i in str:
+#     if(i.isalpha()):
+#         alphabets+=1
+#     elif(i.isdigit()):
+#         digits+=1   
+# print("In String",str,"no. of digits are",digits,"and no. of alphabets are",alphabets)
