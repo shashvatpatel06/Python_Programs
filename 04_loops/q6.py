8bf6179838261948aef2f32dd819512393374d9b
Shashvat Patel <shashvatpatel006@gmail.com>
2025-03-07 16:16:39 +0530

    Create q6.py

---
+++ 04_loops/q6.py
@@ -0,0 +1,10 @@
+# 6.)print the hours with am/pm
+
+# h = int(input("Enter hours:"))
+# dict = {
+#     13:1,14:2,15:3,16:4,17:5,18:6,19:7,20:8,
+#     21:9,22:10,23:11,24:12}
+# if(0<h<12):
+#     print(h,"am")
+# elif(12<=h<24):
+#     print(dict[h],"pm")
