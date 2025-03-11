2693531b3dcd6e17b9da01cb16203e8b568dbb58
Shashvat Patel <shashvatpatel006@gmail.com>
2025-03-11 12:24:48 +0530

    Update q1.py

--- 09_functions/01_f/q1.py
+++ 09_functions/01_f/q1.py
@@ -1,1 +1,11 @@
+d = {"lower":0,"upper":0}
 
+def count_lower_upper(str):
+    for i in str:
+        if(i.islower()):
+            d["lower"] += 1
+        elif(i.isupper()):
+            d["upper"] += 1
+            
+count_lower_upper(str = input("Enter a String:"))
+print(d)
