4f139b112191ef94e088ed32e481fc4fa3b16935
Shashvat Patel <shashvatpatel006@gmail.com>
2025-03-18 12:19:27 +0530

    Update q1.py

--- 09_functions/02_r/q1.py
+++ 09_functions/02_r/q1.py
@@ -1,1 +1,11 @@
+def primeFactors(n,l=[],i=2):
+    if(n<=1):
+        return l
+    elif(n%i==0):
+        l.append(i)
+        return primeFactors(n//i,l,i)
+    else:
+        return primeFactors(n,l,i+1)
 
+n = int(input("Enter a number:"))
+print(primeFactors(n))
