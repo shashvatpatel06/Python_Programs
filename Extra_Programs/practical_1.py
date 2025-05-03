8952739ed608d9e2123ef819e7fd9a32dc5002f4
Shashvat Patel <shashvatpatel006@gmail.com>
2025-05-03 14:22:02 +0530

    Update practical_1.py

--- Extra_Programs/practical_1.py
+++ Extra_Programs/practical_1.py
@@ -1,22 +1,18 @@
-def prime(n):
+def isPrime(n):
     for i in range(2,n):
         if(n%i==0):
             return False
     return True
 s = "Welcome"
 p=[]
-for i in range(5,100):
-    if(prime(i)):
+i=5
+while(len(p)<=len(s)):
+    if(isPrime(i)):
         p.append(i)
-print(p)
-l=[]
-for i in range(0,len(s)):
-    l.append(chr(ord(s[i])+(pow(-1,i)*p[i])))
-    
-print     
+    i+=1    
 s1=''
-for i in range(0,len(l)):
-    s1+=l[i]
+for i in range(0,len(s)):
+    s1+=chr(ord(s[i])+(pow(-1,i)*p[i]))
 print(s1)
 '''
 print(chr(ord('W')+5))
@@ -26,40 +22,8 @@ print(chr(ord('c')-13))
 print(chr(ord('o')+17))
 print(chr(ord('m')-19))
 print(chr(ord('e')+23))
-
-def prime(n):
-    for i in range(2,n):
-        if(n%i==0):
-            return False
-    return True
-s = "Welcome"
-p=[]
-for i in range(5,100):
-    if(prime(i)):
-        p.append(i)
-print(p)
-l=[]
-    
-t=p[0]
-m=1
-for i in range(0,len(s)):
-    for j in range(t,100):
-        if(m==1):
-            if(prime(j)):
-                l.append(chr(ord(s[i])+j))
-                m=2
-                t=p[i+1]
-                break
-        if(m==2):
-            if(prime(j)):
-                l.append(chr(ord(s[i])-j))
-                m=1
-                t=p[i+1]
-                break
-s1=''
-for i in range(0,len(l)):
-    s1+=l[i]
-print(s1)
 '''
+
+
 
 
